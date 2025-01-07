35a60e712e1d8e741a672a1787f95a104f176912
Jerry Lokoroi <jerrylokoroi@gmail.com>
2025-01-07 10:51:03 +0300

    Getting Acquainted with ASP.NET Core

--- RecipeApp.API/Controllers/WeatherForecastController.cs
+++ RecipeApp.API/Controllers/WeatherForecastController.cs
@@ -12,7 +12,7 @@ namespace RecipeApp.API.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
-
+         
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
